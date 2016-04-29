7640119c51906eac51b8636a77904c17342022fb
David MacKintosh <dgawmackintosh@gmail.com>
2016-04-29 11:15:45 +0100

    Completes hashes

--- warehouse_picker_funcs.rb
+++ warehouse_picker_funcs.rb
@@ -1,4 +1,10 @@
+warehouse_array = []
 
+rack_a = { a1: "needle", a2: "stop sign", a3: "blouse", a4: "hanger", a5: "rubber duck", a6: "shovel", a7: "bookmark", a8: "model car", a9: "glow stick", a10: "rubber band" }
+
+rack_c = { c1: "rusty nail", c2: "drill press", c3: "chalk", c4: "word search", c5: "thermometer", c6: "face wash", c7: "paint brush", c8: "candy wrapper", c9: "shoe lace", c10: "leg warmers" }
+
+rack_b = { b1: "tyre swing", b2: "sharpie", b3: "picture frame", b4: "photo album", b5: "nail filer", b6: "tooth paste", b7: "bath fizzers", b8: "tissue box", b9: "deodorant", b10: "cookie jar" }
 
 
 
