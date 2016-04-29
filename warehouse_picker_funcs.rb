f16a8df0b23700744890379b79f628c29c88f6bc
David MacKintosh <dgawmackintosh@gmail.com>
2016-04-29 11:23:21 +0100

    Fixes hashes

--- warehouse_picker_funcs.rb
+++ warehouse_picker_funcs.rb
@@ -1,12 +1,12 @@
-warehouse_array = []
+warehouse_array = {
 
-rack_a = { a1: "needle", a2: "stop sign", a3: "blouse", a4: "hanger", a5: "rubber duck", a6: "shovel", a7: "bookmark", a8: "model car", a9: "glow stick", a10: "rubber band" }
+rack_a = { a10: "rubber band", a9: "glow stick", a8: "model car", a7: "bookmark",  a6: "shovel",  a5: "rubber duck", a4: "hanger", a3: "blouse", a2: "stop sign", a1: "needle" }
 
 rack_c = { c1: "rusty nail", c2: "drill press", c3: "chalk", c4: "word search", c5: "thermometer", c6: "face wash", c7: "paint brush", c8: "candy wrapper", c9: "shoe lace", c10: "leg warmers" }
 
 rack_b = { b1: "tyre swing", b2: "sharpie", b3: "picture frame", b4: "photo album", b5: "nail filer", b6: "tooth paste", b7: "bath fizzers", b8: "tissue box", b9: "deodorant", b10: "cookie jar" }
 
-
+}
 
 def item_in_bay()
 
