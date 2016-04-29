bb82ce3b0957a951f3562eb84e94422d1a945343
David MacKintosh <dgawmackintosh@gmail.com>
2016-04-29 11:57:10 +0100

    Switches hashes and arrays

--- specs/warehouse_picker_spec.rb
+++ specs/warehouse_picker_spec.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require ‘minitest/rg’
 require_relative '../warehouse_picker_funcs.rb'
 
 class TestWarehouse < Minitest::Test
--- warehouse_picker_funcs.rb
+++ warehouse_picker_funcs.rb
@@ -1,15 +1,48 @@
-warehouse_array = {
+WAREHOUSE = {
 
-rack_a = { a10: "rubber band", a9: "glow stick", a8: "model car", a7: "bookmark",  a6: "shovel",  a5: "rubber duck", a4: "hanger", a3: "blouse", a2: "stop sign", a1: "needle" }
+rack_a: [ 
+"rubber band",
+"glow stick",
+"model car",
+"bookmark", 
+"shovel",
+"rubber duck",
+"hanger",
+"blouse",
+"stop sign",
+"needle" 
+]
 
-rack_c = { c1: "rusty nail", c2: "drill press", c3: "chalk", c4: "word search", c5: "thermometer", c6: "face wash", c7: "paint brush", c8: "candy wrapper", c9: "shoe lace", c10: "leg warmers" }
+rack_c: [ 
+"rusty nail",
+"drill press",
+"chalk",
+"word search",
+"thermometer",
+"face wash",
+"paint brush",
+"candy wrapper",
+"shoe lace",
+"leg warmers"
+]
 
-rack_b = { b1: "tyre swing", b2: "sharpie", b3: "picture frame", b4: "photo album", b5: "nail filer", b6: "tooth paste", b7: "bath fizzers", b8: "tissue box", b9: "deodorant", b10: "cookie jar" }
+rack_b: [
+"tyre swing",
+"sharpie",
+"picture frame",
+"photo album",
+"nail filer",
+"tooth paste",
+"bath fizzers",
+"tissue box",
+"deodorant",
+"cookie jar"
+]
 
 }
 
 def item_in_bay()
-
+  WAREHOUSE[]
 end
 
 
